eb259a7ea2ec62d5a05af870c44d8126928e7629
Kimberly Tran <ktran5@cs.ualberta.ca>
2022-11-01 21:42:55 -0600

    started server.py

--- server.py
+++ server.py
@@ -79,21 +79,25 @@ def hello():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
+    myWorld.update(entity)
     return None
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    world = myWorld.world()
+    return world
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    representation = myWorld.get(entity)
+    return representation
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
+    myWorld.clear()
     return None
 
 if __name__ == "__main__":
